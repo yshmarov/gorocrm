3952222c889d8cbf2232a5cee70c1c5266e53159
Yaro <yshmarov@gmail.com>
2020-06-15 11:08:36 +0000

    tenants styling and validation

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,28 +1,20 @@
 class TenantsController < ApplicationController
   before_action :set_tenant, only: [:show, :edit, :update, :destroy]
 
-  # GET /tenants
-  # GET /tenants.json
   def index
     @tenants = Tenant.all
   end
 
-  # GET /tenants/1
-  # GET /tenants/1.json
   def show
   end
 
-  # GET /tenants/new
   def new
     @tenant = Tenant.new
   end
 
-  # GET /tenants/1/edit
   def edit
   end
 
-  # POST /tenants
-  # POST /tenants.json
   def create
     @tenant = Tenant.new(tenant_params)
 
@@ -37,8 +29,6 @@ class TenantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tenants/1
-  # PATCH/PUT /tenants/1.json
   def update
     respond_to do |format|
       if @tenant.update(tenant_params)
@@ -51,8 +41,6 @@ class TenantsController < ApplicationController
     end
   end
 
-  # DELETE /tenants/1
-  # DELETE /tenants/1.json
   def destroy
     @tenant.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class TenantsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_tenant
       @tenant = Tenant.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def tenant_params
       params.require(:tenant).permit(:name)
     end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,5 @@
 class Tenant < ApplicationRecord
+  
+  validates :name, presence: true, uniqueness: true
+  
 end
