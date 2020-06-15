592e2255f80fea73999495874c849e10458daf9e
Yaro <yshmarov@gmail.com>
2020-06-15 11:24:24 +0000

    user-member-tenant relationships

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,9 @@
 class Member < ApplicationRecord
   belongs_to :user
   belongs_to :tenant
+
+  validates :tenant_id, presence: true
+  validates_uniqueness_of :user_id, scope: :tenant_id
+
+
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,12 @@
 class Tenant < ApplicationRecord
   
   validates :name, presence: true, uniqueness: true
+
+  has_many :members
+  has_many :users, through: :members
+
+  def to_s
+    name
+  end
   
 end
