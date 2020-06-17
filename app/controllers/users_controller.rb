f9607b9019eefc9338c424c0d8159745fcc51d4a
Yaro <yshmarov@gmail.com>
2020-06-17 18:09:25 +0000

    if the user has not accepted the invitation, he can be re-invited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
     @users = User.all
   end
   
+  def resend_invitation
+    @user = User.find(params[:id])
+    if @user.invitation_accepted_at.present?
+      redirect_to users_path, alert: "User with email #{@user.email} has already accepted the invitation"
+    else
+      @user.invite!
+      redirect_to users_path, notice: "Invitation resent to #{@user.email}"
+    end
+  end
+  
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do
+      patch :resend_invitation
+    end
+  end
 
   resources :tenants do
     get :my, on: :collection
