16b47952386a2d72ead1cd8443fd60e47a486106
Yaro <yshmarov@gmail.com>
2020-06-22 11:04:13 +0000

    global and tenant-specific views

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :user
-  belongs_to :tenant
+  #belongs_to :tenant #acts_as_tenant includes this
 
   validates :tenant_id, presence: true
   validates_uniqueness_of :user_id, scope: :tenant_id
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,6 +1,7 @@
 class Tenant < ApplicationRecord
   
   validates :name, presence: true, uniqueness: true
+  validates :name, length: { in: 2..20 }
 
   has_many :members
   has_many :users, through: :members
