4138e770873b63dd666c0aa6df486b7e472025f1
Yaro <yshmarov@gmail.com>
2020-07-16 22:05:46 +0000

    stylesheet_pack_tag replaces stylesheet_link_tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,10 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
+
+require("stylesheets/application.scss")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
