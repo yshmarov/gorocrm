7ddc9b90c0361106a613fa5fc8e480e96d2b20c0
Yaro <yshmarov@gmail.com>
2020-07-16 22:13:03 +0000

    add fontawesome icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import "bootstrap/dist/css/bootstrap";
 
 require("stylesheets/application.scss")
 
+import "@fortawesome/fontawesome-free/js/all"
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
