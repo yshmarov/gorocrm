bfaa80e5f0b577aeb32ed17466662cfe64fbe2c2
Yaro <yshmarov@gmail.com>
2020-07-23 19:10:11 +0000

    Basic validation for Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :tenant
+  
+  validates :first_name, :last_name, :phone_number, :email, presence: true
+  #validates_uniqueness_to_tenant :first_name, :last_name, :phone_number, :email
+  validates_uniqueness_of :phone_number, scope: :tenant_id
+
 end
