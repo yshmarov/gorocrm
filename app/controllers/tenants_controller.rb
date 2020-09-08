dc06008221b8dc3757d421c0630f51b9992a41fa
Yaro <yshmarov@gmail.com>
2020-09-08 14:51:25 +0000

    minor styling (no video)

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -28,8 +28,8 @@ class TenantsController < ApplicationController
   def switch
     if current_user.tenants.include?(@tenant)
       current_user.update_attribute(:tenant_id, @tenant.id)
-      redirect_to my_tenants_path, notice: "Switched to tenant: #{current_user.tenant.name}"
-      #redirect_to tenant_path(current_user.tenant), notice: "Switched to tenant: #{current_user.tenant.name}"
+      #redirect_to my_tenants_path, notice: "Switched to tenant: #{current_user.tenant.name}"
+      redirect_to tenant_path(current_user.tenant), notice: "Switched to tenant: #{current_user.tenant.name}"
     else
       redirect_to my_tenants_path, alert: "You are not authorized to access tenant: #{@tenant.name}"
     end
