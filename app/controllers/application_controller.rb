6dfeaa6a4c81dd1eaaecc310305adea22846b103
Yaro <yshmarov@gmail.com>
2020-09-29 19:22:18 +0000

    select language for session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,22 @@ class ApplicationController < ActionController::Base
 
   #include SetTenant #set ActsAsTenant.current_tenant
 
+  before_action :set_locale
+  
+  private
+  
+  def set_locale
+    if params['locale'].present?
+      language = params['locale'].to_sym
+      session['locale'] = language
+      redirect_to root_path
+    elsif session['locale'].present?
+      language = session['locale']
+    else
+      language = 'en'
+    end
+    
+    I18n.locale = language
+  end
+
 end
