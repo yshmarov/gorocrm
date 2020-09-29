660b3e6b74b97aa80fae1243d106c6306b73a6db
Yaro <yshmarov@gmail.com>
2020-09-29 19:01:38 +0000

    add locales

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,12 @@ module Saascorsego
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :es]
+     
+    # Set default locale to something other than :en
+    I18n.default_locale = :en
+
   end
 end
