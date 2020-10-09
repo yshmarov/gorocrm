1de36e73fb41622879db93500d0f3d9547e3701f
Yaro <yshmarov@gmail.com>
2020-10-09 19:54:40 +0000

    routes - superadmin namespace

--- app/controllers/superadmin/users_controller.rb
+++ app/controllers/superadmin/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Superadmin::UsersController < ApplicationController
   
   def index
     @users = User.includes(:members, :tenants, members: [:tenant])
