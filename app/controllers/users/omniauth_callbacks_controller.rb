2192596e1897eccac9335997e6b36f51cd76b9f8
Yaro <yshmarov@gmail.com>
2020-10-12 17:33:01 +0000

    p2 connect multiple oAuth providers to one user (refactoring controller)

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,23 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def google_oauth2
-    handle_auth "Google"
-  end
+  Devise.omniauth_configs.keys.each do |provider|
+    #omni callback routes us to this action
+    #this action will be automatically named as the omni provider [:github, :google_oauth2]
+    define_method provider do
+      #at this point the omni callback works and we get provider data
+      auth = request.env['omniauth.auth']
 
-  def github
-    handle_auth "Github"
-  end
-
-  def handle_auth(kind)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session['devise.github_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      if auth.nil?
+        redirect_to root_path, alert: "No data received. Please try again"
+      else
+        @user = User.from_omniauth(auth)
+    
+        if @user.persisted?
+          flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: provider
+          sign_in_and_redirect @user, event: :authentication
+        else
+          session['devise.github_data'] = auth.except('extra')
+          redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+        end
+      end
     end
+  end
+
+  def failure
+    redirect_to root_path, alert: "Something went wrong. Please try again"
   end
 
 end
