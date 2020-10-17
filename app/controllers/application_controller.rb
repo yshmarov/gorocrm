f0573a1ee26b33565468cd6a86eb27a8663b1076
Yaro <yshmarov@gmail.com>
2020-10-17 13:43:04 +0000

    clean up application_controller - set_locale concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  #include SetTenant #set ActsAsTenant.current_tenant
+  include SetLocale
 
-  before_action :set_locale
-  
-  private
-  
-  def set_locale
-    if params['locale'].present?
-      language = params['locale'].to_sym
-      session['locale'] = language
-      if user_signed_in?
-        current_user.update(language: language)
-      end
-      redirect_to root_path
-    elsif session['locale'].present?
-      language = session['locale']
-    else
-      language = I18n.default_locale
-    end
-    
-    if user_signed_in? && current_user.language.present?
-      language = current_user.language
-    end
-
-    if I18n.available_locales.map(&:to_s).include?(language)
-      I18n.locale = language
-    else
-      I18n.locale = I18n.default_locale
-    end
-  end
-
+  #devise
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
---
+++ app/controllers/concerns/set_locale.rb
@@ -0,0 +1,36 @@
+module SetLocale
+  extend ActiveSupport::Concern
+
+  included do
+
+  before_action :set_locale
+  
+  private
+  
+    def set_locale
+      if params['locale'].present?
+        language = params['locale'].to_sym
+        session['locale'] = language
+        if user_signed_in?
+          current_user.update(language: language)
+        end
+        redirect_to root_path
+      elsif session['locale'].present?
+        language = session['locale']
+      else
+        language = I18n.default_locale
+      end
+      
+      if user_signed_in? && current_user.language.present?
+        language = current_user.language
+      end
+  
+      if I18n.available_locales.map(&:to_s).include?(language)
+        I18n.locale = language
+      else
+        I18n.locale = I18n.default_locale
+      end
+    end
+
+  end
+end
