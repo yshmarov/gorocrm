7822eed267a50ecc0cd836ffe6c9e84d16405472
Yaro <yshmarov@gmail.com>
2020-10-30 08:37:46 +0000

    ActsAsTenant for subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,15 @@
 class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: [:destroy]
 
+  include SetTenant #set ActsAsTenant.current_tenant
+  #include RequireTenant #no current_tenant = no access to entire controller
+  include SetCurrentMember #for role-based authorization
+
+  before_action :require_tenant_admin
+
   def create
     plan = Plan.find(params[:plan])
-    @subscription = Subscription.create(plan: plan, tenant: current_user.tenant, ends_at: Time.now)
+    @subscription = Subscription.create(plan: plan, ends_at: Time.now)
 
     if @subscription.save
       redirect_to current_user.tenant, notice: 'Subscription was successfully created.'
@@ -22,6 +28,12 @@ class SubscriptionsController < ApplicationController
   private
     def set_subscription
       @subscription = Subscription.find(params[:id])
+    end
+
+    def require_tenant_admin
+      unless @current_member.admin?
+        redirect_to tenant_path(current_tenant), alert: "You are not authorized to manage subscriptions"
+      end
     end
 
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,9 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :plan
-  belongs_to :tenant
+  acts_as_tenant(:tenant)
   has_many :charges, dependent: :nullify
-  
-  
+
   validates :tenant_id, uniqueness: true
   validates :ends_at, presence: true
 
