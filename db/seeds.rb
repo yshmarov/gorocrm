4d6a2c0859594a899f9ebf0da6bdb3beaca22bb8
Yaro <yshmarov@gmail.com>
2020-10-30 10:56:18 +0000

    add plans to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@ User.update_all confirmed_at: DateTime.now
 #User.first.toggle(:superadmin) #will not work because superadmin is not a whitelisted param (good for security)
 #User.first.update!(superadmin: true) #will not work because superadmin is not a whitelisted param (good for security)
 #Member.first.update!(admin: true)
+
+Plan.create(name: "solo", amount: 0, currency: "usd", interval: "forever", max_members: 1)
+Plan.create(name: "team", amount: 1000, currency: "usd", interval: "month", max_members: 15)
+Plan.create(name: "team -16%", amount: 10000, currency: "usd", interval: "year", max_members: 15)
