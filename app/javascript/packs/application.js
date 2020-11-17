f0805fae8f94995da2553fa82486dd566bd997c1
Yaro <yshmarov@gmail.com>
2020-11-17 15:37:23 +0000

    add service_workers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,23 @@ require("stylesheets/application.scss")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    console.log('ServiceWorker registered: ', registration);
+
+    var serviceWorker;
+    if (registration.installing) {
+      serviceWorker = registration.installing;
+      console.log('Service worker installing.');
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting;
+      console.log('Service worker installed & waiting.');
+    } else if (registration.active) {
+      serviceWorker = registration.active;
+      console.log('Service worker active.');
+    }
+  }).catch(registrationError => {
+    console.log('Service worker registration failed: ', registrationError);
+  });
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,8 @@
-const { environment } = require('@rails/webpacker')
+const { resolve } = require('path');
+const { config, environment, Environment } = require('@rails/webpacker');
+const WebpackerPwa = require('webpacker-pwa');
+new WebpackerPwa(config, environment);
+
 const webpack = require("webpack")
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
@@ -6,4 +10,5 @@ environment.plugins.append("Provide", new webpack.ProvidePlugin({
     'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default']
   }))
+
 module.exports = environment
