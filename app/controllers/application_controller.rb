06836e57d7ba187a3499c8b21514b8044a1c4267
Yaro <yshmarov@gmail.com>
2020-11-23 00:42:20 +0000

    dark mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,21 @@ class ApplicationController < ActionController::Base
   include SetLocale
   include SetTimeZone
 
+  before_action :set_theme
+  
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      if ["light", "dark"].include?(params[:theme])
+        # session[:theme] = theme
+        cookies[:theme] = theme
+      else
+        cookies[:theme] = "light"
+      end
+      redirect_to (request.referrer || root_path), notice: "The css for dark mode is still experimental"
+    end
+  end
+
   # devise
   def after_sign_in_path_for(resource)
     user_path(resource)
