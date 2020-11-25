61b27c13256d9306722e5718b44b0c0c1aa472c8
Yaro <yshmarov@gmail.com>
2020-11-25 16:50:42 +0000

    include associations in superadmin views, i18n work

--- app/controllers/superadmin_controller.rb
+++ app/controllers/superadmin_controller.rb
@@ -3,19 +3,19 @@ class SuperadminController < ApplicationController
   end
 
   def tenants
-    @tenants = Tenant.includes(:members, :users, members: [:user]).order(created_at: :desc)
+    @tenants = Tenant.includes(:members, :users, :subscription, subscription: [:plan], members: [:user]).order(created_at: :desc)
     render "tenants/index"
   end
 
   def users
-    @users = User.includes(:members, :tenants, members: [:tenant]).order(created_at: :desc)
+    @users = User.includes(:identities, :members, :tenants, members: [:tenant]).order(created_at: :desc)
   end
 
   def charges
-    @charges = Charge.all.order(created_at: :desc)
+    @charges = Charge.includes(:tenant).order(created_at: :desc)
   end
 
   def subscriptions
-    @subscriptions = Subscription.all.order(created_at: :desc)
+    @subscriptions = Subscription.includes(:tenant, :plan).order(created_at: :desc)
   end
 end
