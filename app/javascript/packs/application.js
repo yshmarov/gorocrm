5310f81cbaa1cb5a2744956ec368190dd34d72ac
Yaro <yshmarov@gmail.com>
2021-01-24 12:24:57 +0000

    toastr js for flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import "bootstrap/dist/css/bootstrap";
 
 import "@fortawesome/fontawesome-free/css/all"
 
+global.toastr = require("toastr")
+
 require("stylesheets/application.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
