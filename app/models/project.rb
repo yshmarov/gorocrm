9e496b73f2edc35e19f7a6c281b223ebae5d0dc7
Yaro <yshmarov@gmail.com>
2021-02-03 16:43:38 +0000

    improve project form, task calendar

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,12 @@
 class Project < ApplicationRecord
   acts_as_tenant(:tenant)
   belongs_to :client
-  validates :client, :name, presence: true
+  validates :client, :payment_type, :name, presence: true
+
+  PAYMENT_TYPES = [:hourly, :fixed]
+  def self.payment_types
+    PAYMENT_TYPES.map { |payment_type| [payment_type, payment_type] }
+  end
 
   def to_s
     name
--- app/models/task.rb
+++ app/models/task.rb
@@ -12,4 +12,26 @@ class Task < ApplicationRecord
     STATUSES.map { |status| [status, status] }
   end
 
+  # for calendar
+  def calendar_date
+    if done_at.present?
+      done_at
+    else
+      created_at
+    end
+  end
+
+  # for calendar
+  def status_color
+    if status == "planned"
+      "danger"
+    elsif status == "progress"
+      "warning"
+    elsif status == "done"
+      "success"
+    else
+      "danger"
+    end
+  end
+
 end
