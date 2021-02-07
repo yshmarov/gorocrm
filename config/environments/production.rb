b00ca4de70d4d2b4acbabb5f5ca7e2e6144fd935
Yaro <yshmarov@gmail.com>
2021-02-07 15:58:54 +0000

    gorocrom production mailer is heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {host: "gorocrm.corsego.com", protocol: "https"}
+  config.action_mailer.default_url_options = {host: "gorocrm.herokuapp.com", protocol: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
