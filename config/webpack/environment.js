4a06ecc5efcd4a6c6bd5828b635a04bac9aec476
Yaro <yshmarov@gmail.com>
2021-04-02 20:45:14 +0000

    bootstrap 5 basics

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,2 @@
-const { resolve } = require('path');
-const { config, environment, Environment } = require('@rails/webpacker');
-const WebpackerPwa = require('webpacker-pwa');
-new WebpackerPwa(config, environment);
-
-const webpack = require("webpack")
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'default']
-  }))
-
+const { environment } = require('@rails/webpacker')
 module.exports = environment
